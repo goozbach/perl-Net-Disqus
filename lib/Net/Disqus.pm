67b896e81635c67c5eadb0f5d8ed0cf84c968fac
Derek Carter <goozbach@friocorte.com>
2009-08-01 15:29:53 -0400

    updated licence to better than same as perl

--- lib/Net/Disqus.pm
+++ lib/Net/Disqus.pm
@@ -94,9 +94,18 @@ L<http://wiki.github.com/goozbach/perl-Net-Disqus>
 
 Copyright 2009 Derek Carter, all rights reserved.
 
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
+This program is free software; you can redistribute it and/or
+modify it under the terms of either:
 
+=over 4
+
+=item * the GNU General Public License as published by the Free
+Software Foundation; either version 1, or (at your option) any
+later version, or
+
+=item * the Artistic License version 2.0.
+
+=back
 
 =cut
 
