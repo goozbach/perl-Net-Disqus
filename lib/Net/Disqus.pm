289c438034c0ddc327e197e91586252156da30e3
Derek Carter <goozbach@friocorte.com>
2009-08-03 23:02:04 -0400

    cutting the overhead doc to make perldoc per module, net-disqus

--- lib/Net/Disqus.pm
+++ lib/Net/Disqus.pm
@@ -5,7 +5,7 @@ use strict;
 
 =head1 NAME
 
-Net::Disqus - The great new Net::Disqus!
+Net::Disqus - A OOP interface to the public Disqus API
 
 =head1 VERSION
 
@@ -18,35 +18,348 @@ our $VERSION = '0.01';
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+This module is the OOP interface to the public Disqus API. It allows for easy access to post/forums/threads
+that have been posted to Disqus.  I wrote it for the purposes of including the comments from disqus directly
+into my website instead of loading them via JavaScript
 
-Perhaps a little code snippet.
+=head1 USAGE
 
     use Net::Disqus;
 
     my $foo = Net::Disqus->new();
-    ...
 
-=head1 EXPORT
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+=head1 API method Functions
 
-=head1 FUNCTIONS
+=head2 C<create_post>
 
-=head2 function1
+=over 2
 
-=cut
+=item * Method -- POST
 
-sub function1 {
+=item * Key -- Forum Key
+
+=back 
+
+=head3 Arguments
+
+=over 2
+
+=item * Required
+
+=over 4
+
+=item * C<thread_id> -- The ID of the thread to create the post on.
+
+=item * C<message> -- The message of the post.
+
+=item * C<author_name> -- The creator's name.
+
+=item * C<author_email> -- The creator's email address.
+
+=back
+
+=item * Optional
+
+=over 4
+
+=item * C<parent_post> -- The ID of the parent post, for threaded comments.
+
+=item * C<created_at> -- The time the post was created. in format: %Y-%m-%dT%H:%M. If ommited the current time will be used
+
+=item * C<author_url> -- The author's homepage
+
+=item * C<ip_address> -- The author's IP address
+
+=back
+
+=back
+
+=head3 Returns
+
+On success, the L<Net::Disqus::Post> object just created.
+
+=cut 
+sub create_post {
+
 }
 
-=head2 function2
+=head2 C<get_forum_list>
+
+=over 2
+
+=item * Method -- GET
+
+=item * Key -- The user's C<user_api_key>
+
+=back
+
+=head3 Arguments
+
+None
+
+=head3 Returns
+
+A list of L<Net::Disqus::Forum objects> the user owns.
 
 =cut
+sub get_forum_list {
 
-sub function2 {
 }
+
+=head2 C<get_forum_api_key>
+
+=over 2
+
+=item * Method -- GET
+
+=item * Key -- The user's C<user_api_key>
+
+=back
+
+=head3 Arguments
+
+=over 2
+
+=item * Required
+
+=over 4
+
+=item * C<forum_id> -- The ID of the forum being requested.
+
+=back
+
+=back
+
+=head3 Returns
+
+a string containing the forum key.
+
+=cut
+sub get_forum_api_key {
+
+}
+
+=head2 C<get_thread_list>
+
+=over 2
+
+=item * Method -- GET
+
+=item * Key -- Forum Key
+
+=item * Key -- The user's C<user_api_key>
+
+=back
+
+=head3 Arguments
+
+None
+
+=head3 Returns
+
+A list of L<Net::Disqus::Thread> objects belonging to the given forum.
+
+=cut 
+sub get_thread_list {
+
+}
+
+=head2 C<get_num_posts>
+
+=over 2
+
+=item * Method -- GET
+
+=item * Key -- Forum Key
+
+=item * Key -- The user's C<user_api_key>
+
+=back
+
+=head3 Arguments
+
+=over 2
+
+=item * Required
+
+=over 4
+
+=item * C<thread_ids> -- A comma delimited list of thread IDs belonging to the forum.
+
+=back 
+
+=back
+
+=head3 Returns
+
+An pseudo object mapping each C<thread_id> to two numbers: visible comments, total comments.
+
+=cut
+sub get_num_posts {
+
+}
+
+=head2 C<get_thread_by_url>
+
+=over 2
+
+=item * Method -- GET
+
+=item * Key -- Forum Key
+
+=item * Key -- The user's C<user_api_key>
+
+=back
+
+=head3 Arguments
+
+=over 2
+
+=item * Required
+
+=over 4
+
+=item * C<url> -- The url to check for a thread. --TODO discover if URI encoded or not.
+
+=back
+
+=back
+
+=head3 Returns
+
+=over 2
+
+=item * On Success -- A L<Net::Disqus::Thread> object.
+
+=item * On Failure -- NULL
+
+=back
+
+=cut 
+sub get_thread_by_url {
+
+}
+
+=head2 C<get_thread_posts>
+
+=over 2
+
+=item * Method -- GET
+
+=item * Key -- Forum Key
+
+=item * Key -- The user's C<user_api_key>
+
+=back
+
+=head3 Arguments
+
+=over 2
+
+=item * Required
+
+=over 4
+
+=item * C<thread_id> -- The ID of the thread for which you are requesting the posts.
+
+=back
+
+=back
+
+=head3 Returns
+
+A list of L<Net::Disqus::Post> objects for the given thread/forum.
+
+=cut
+sub get_thread_posts {
+
+}
+
+=head2 C<thread_by_identifier>
+
+=over 2
+
+=item * Method -- POST
+
+=item * Key -- Forum Key
+
+=item * Key -- The user's C<user_api_key>
+
+=back
+
+=head3 Arguments
+
+=over 2
+
+=item * Required
+
+=over 4
+
+=item * C<title> -- The title of the thread to possibly be created
+
+=item * C<identifier> -- A string of your choosing
+
+=back
+
+=back
+
+=head3 Returns
+
+An pseudo object with two keys: 
+
+=over 2
+
+=item * C<thread> -- The Net::Disqus::Thread object corresponding to the identifier
+
+=item * C<created> -- A boolean which indicates whether the thread was created as a result of this method call. If created, it will have the specified title.
+
+=back
+
+=cut
+sub thread_by_identifier {
+
+}
+
+=head2 C<update_thread>
+
+=over 2
+
+=item * Method -- POST
+
+=item * Key -- Forum Key
+
+=item * Key -- The user's C<user_api_key>
+
+=back
+
+=head3 Arguments
+
+=over 2
+
+=item * Required
+
+=over 4
+
+=item * C<thread_id> -- The title of the thread to possibly be created
+
+=back
+
+=item * Optional -- Any of:
+
+=over 4
+
+=item * C<title> -- update the title attribute of the thread
+
+=item * C<slug> -- update the slug attribute of the thread
+
+=item * C<url> -- update the url attribute of the thread
+
+=item * C<allow_comments> -- update the allow_comments attribute of the thread
+
+=back
+
+=back 
 
 =head1 AUTHOR
 
@@ -54,12 +367,8 @@ Derek Carter, C<< <goozbach at friocorte.com> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-net-disqus at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Net-Disqus>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
+Please report any bugs or feature requests through the web interface at L<http://github.com/goozbach/perl-Net-Disqus/issues>.
+I will be notified, and then you'll automatically be notified of progress on your bug as I make changes.
 
 =head1 SUPPORT
 
